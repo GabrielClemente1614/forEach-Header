5c5df1f3c587f3165b45d25eeb84ee02d63d19a1
Gabriel Clemente <gabrielclementearaujo1610@gmail.com>
2024-09-09 17:22:42 -0300

    upt 2.5 bug

--- api.js
+++ api.js
@@ -1,9 +1,11 @@
-// JavaScript
+// api.js
 
+// Seleção de elementos
 var elements = document.querySelectorAll('.a'); 
 var eluments = document.querySelectorAll(".b");
 
-window.addEventListener("scroll", function() {
+// Função para atualizar classes de scroll
+function handleScroll() {
     var header = document.querySelector('header');
     header.classList.toggle("sticky", window.scrollY > 0);
 
@@ -14,25 +16,30 @@ window.addEventListener("scroll", function() {
     eluments.forEach(function(element) {
         element.classList.toggle("txtAni", window.scrollY > 0);
     });
-});
+}
 
+// Adiciona o evento de scroll
+window.addEventListener("scroll", handleScroll);
 
+// Seleção do menu e botão
 var menu = document.querySelector(".cont-mobile");
 var btnMenu = document.querySelector(".btn-botao");
 
-btnMenu.addEventListener("click", function() {
-    menu.classList.toggle("active");
-    document.body.classList.toggle("no-scroll", menu.classList.contains("active"), btnMenu.classList.toggle("img"));
-});
+// Função para alternar o menu
+function toggleMenu() {
+    var isActive = menu.classList.toggle("active");
+    document.body.classList.toggle("no-scroll", isActive);
+    btnMenu.classList.toggle("img", isActive);
+}
 
-// Adiciona um listener para fechar o menu se você clicar fora dele
+// Adiciona o evento de click no botão do menu
+btnMenu.addEventListener("click", toggleMenu);
+
+// Fecha o menu se clicar fora dele
 document.addEventListener('click', function(event) {
     if (!menu.contains(event.target) && !btnMenu.contains(event.target)) {
         menu.classList.remove('active');
         document.body.classList.remove("no-scroll");
+        btnMenu.classList.remove('img'); // Garantir que a classe do botão também é removida
     }
 });
-
-
-
-// JavaScript
