27b3c094df9556cd0f28a1e6f81b338a6a9a10ce
Gabriel Clemente <gabrielclementearaujo1610@gmail.com>
2024-09-09 21:37:07 -0300

    upt 3.0

--- api.js
+++ api.js
@@ -43,3 +43,46 @@ document.addEventListener('click', function(event) {
         btnMenu.classList.remove('img'); // Garantir que a classe do botão também é removida
     }
 });
+
+var res1 = document.querySelector('.res1')
+var res2 = document.querySelector('.res2')
+var res3 = document.querySelector('.res3')
+var btn1 = document.querySelector('.btn-1')
+var btn2 = document.querySelector('.btn-2')
+var btn3 = document.querySelector('.btn-3')
+
+
+
+btn1.addEventListener("click" , function() {
+    if (res1.style.display === "block") {
+        document.querySelector('.img-a').src = "chevron_right_16dp_5F6368_FILL0_wght400_GRAD0_opsz20.png";
+        res1.style.display = "none";
+    } else {
+        res1.style.display = "block"
+        document.querySelector('.img-a').src = "close_16dp_5F6368_FILL0_wght400_GRAD0_opsz20.png";
+    }
+})
+
+btn2.addEventListener("click" , function() {
+    if (res2.style.display === "block") {
+        document.querySelector('.img-b').src = "chevron_right_16dp_5F6368_FILL0_wght400_GRAD0_opsz20.png";
+        res2.style.display = "none";
+    } else {
+        res2.style.display = "block"
+        document.querySelector('.img-b').src = "close_16dp_5F6368_FILL0_wght400_GRAD0_opsz20.png";
+    }
+})
+
+btn3.addEventListener("click" , function() {
+    if (res3.style.display === "block") {
+        document.querySelector('.img-c').src = "chevron_right_16dp_5F6368_FILL0_wght400_GRAD0_opsz20.png";
+        res3.addEventListener.toogle("animaTXT")
+        res3.style.display = "none";
+    } else {
+        res3.style.display = "block"
+        document.querySelector('.img-c').src = "close_16dp_5F6368_FILL0_wght400_GRAD0_opsz20.png";
+    }
+})
+
+
+
